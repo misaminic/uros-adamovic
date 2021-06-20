1dd529601d0189b8ae8fd056b8becc9b3c319e7a
misaminic <misaminic@gmail.com>
2021-06-20 21:44:19 +0200

    adding form 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/Link';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import styled from 'styled-components';
 import { useState, useCallback, useRef, useEffect } from 'react';
